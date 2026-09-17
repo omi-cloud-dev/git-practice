3420592ae67d5b907de0dbc171140773d0b3975c
Omi <Pampari.omprakash@gmail.com>
2026-09-17 09:00:17 +0530

    adding more special variables

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -3,3 +3,5 @@
 echo "All variables passed to the script: $@"
 echo "no of variable passed : $# "
 echo "script name : $0 "
+echo " current working directory : $PWD"
+echo " Home directory of current user ; $HOME"
