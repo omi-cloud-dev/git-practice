6bdcb64959a038f810bca50881cb6d3604f79f14
Omi <Pampari.omprakash@gmail.com>
2026-09-17 09:27:11 +0530

    condidiitons

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -3,7 +3,7 @@
 NUMBER=$1
 
 if ($NUMBER is gt 20)
-    echo("number is greater than 20")
+    echo"number is greater than 20"
    else
-   echo("number is less than 20")
+   echo"number is less than 20"
 fi 
