b06dda67905d7dd3844cbe18191b102b0c8b8094
Omi <Pampari.omprakash@gmail.com>
2026-09-20 17:53:14 +0530

    loops practice

--- 08-installlation.sh
+++ 08-installlation.sh
@@ -8,4 +8,12 @@ if [ $USERID -ne 0 ]
         exit 1
 fi    
 
+dnf list installed git
 
+if [ $? -ne 0 ]
+then
+    echo "git is not installed , going to install git.."
+    dnf install git -y
+else
+    echo "git is already installed "    
+fi
