44adcf4fe449ec69a82a23113274d493503f66a7
Omi <Pampari.omprakash@gmail.com>
2026-09-20 17:47:20 +0530

    loops practice

--- 08-installlation.sh
+++ 08-installlation.sh
@@ -2,16 +2,10 @@
 
 USERID=$(id -u)
 
-CHECK_ROOT(){
-
-    if [ $USERID -ne 0]
+if [ $USERID -ne 0]
     then
         echo "please run the script under root privilges"
         exit 1
     fi    
-}
 
-dnf list installed nginx
 
-if [$? -ne o]
-then dnf install nginx -y
